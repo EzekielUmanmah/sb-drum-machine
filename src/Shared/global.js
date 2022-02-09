15033597cd3c2ccc75f23bbe608bcf672ff256e9
Ezekiel <umanmah234@gmail.com>
2022-02-09 06:43:57 -0500

    environment setup

---
+++ src/Shared/global.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle, css } from 'styled-components';
+import { typography, color } from './styles';
+
+export const bodyStyles = css`
+  font-family: ${typography.type.primary};
+  font-size: ${typography.size.s3}px;
+  color: ${color.darkest};
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  -webkit-tap-highlight-color: transparent;
+  -webkit-overflow-scrolling: touch;
+
+  * {
+    box-sizing: border-box;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: ${typography.weight.regular};
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    ${bodyStyles}
+  }
+`;
